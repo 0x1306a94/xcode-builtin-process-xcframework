b09fd3960a148d33f4555391134aed0b55089a37
0x1306a94 <0x1306a94@gmail.com>
2025-02-05 20:32:48 +0800

    参考swift-build中的实现

--- Sources/xcode-builtin-process-xcframework/Command.swift
+++ Sources/xcode-builtin-process-xcframework/Command.swift
@@ -7,6 +7,7 @@ enum Platform: String, ExpressibleByArgument {
     case watchos
     case tvos
     case xros
+    case driverkit
 }
 
 enum Environment: String, ExpressibleByArgument {
@@ -15,10 +16,10 @@ enum Environment: String, ExpressibleByArgument {
 }
 
 struct Command: ParsableCommand {
-    @Option(name: .long, help: "xcframework path")
-    var xcframework: String
+    @Option(name: .customLong("xcframework"), help: "xcframework path")
+    var source: String
 
-    @Option(name: .long, help: "platform: [ios, macos, watchos, tvos, xros]")
+    @Option(name: .long, help: "platform: [ios, macos, watchos, driverkit, tvos, xros]")
     var platform: Platform
 
     @Option(help: "environment: [simulator, maccatalyst]")
@@ -31,97 +32,100 @@ struct Command: ParsableCommand {
         let fileManager = FileManager.default
         var isDirectory: ObjCBool = false
 
-        if !fileManager.fileExists(atPath: xcframework, isDirectory: &isDirectory) {
-            throw ValidationError("The xcframework path does not exist: \(xcframework)")
+        if !fileManager.fileExists(atPath: source, isDirectory: &isDirectory) {
+            throw ValidationError("The xcframework path does not exist: \(source)")
         }
 
         if !isDirectory.boolValue {
-            throw ValidationError("The specified xcframework is not a folder: \(xcframework)")
+            throw ValidationError("The specified xcframework is not a folder: \(source)")
         }
 
-        if !xcframework.hasSuffix(".xcframework") {
-            throw ValidationError("The path must end with '.xcframework': \(xcframework)")
-        }
-
-        if !fileManager.fileExists(atPath: targetPath, isDirectory: &isDirectory) {
-            throw ValidationError("The targetPath path does not exist: \(targetPath)")
-        }
-
-        if !isDirectory.boolValue {
-            throw ValidationError("The specified targetPath is not a folder: \(targetPath)")
+        if !source.hasSuffix(".xcframework") {
+            throw ValidationError("The path must end with '.xcframework': \(source)")
         }
     }
 
     mutating func run() throws {
-        print("[*] xcframework: \(xcframework)")
-        print("[*] platform: \(platform.rawValue)")
-        if let environment = environment {
-            print("[*] environment: \(environment.rawValue)")
+        let platformDisplayName = "\(platform.rawValue)" + (environment.flatMap { "-\($0.rawValue)" } ?? "")
+
+        print("[*] xcframework: \(source)")
+        print("[*] platform: \(platformDisplayName)")
+
+        let sourcePath = URL(fileURLWithPath: source)
+        let targetPath = URL(fileURLWithPath: targetPath)
+        let plistPath = sourcePath.join("Info.plist")
+        let fs = FileManager.default
+        guard fs.fileExists(atPath: plistPath.path) else {
+            throw ValidationError("There is no Info.plist found at '\(source)'.")
         }
 
-        let rootURL = URL(fileURLWithPath: xcframework)
-        let targetRootURL = URL(fileURLWithPath: targetPath)
-
-        let infoURL = rootURL.appendingPathComponent("Info.plist")
-        let plistData = try Data(contentsOf: infoURL)
+        let plistData = try Data(contentsOf: plistPath)
 
         let decoder = PropertyListDecoder()
-        let xcFrameworkInfo = try decoder.decode(XCFrameworkInfo.self, from: plistData)
+        let xcFrameworkInfo = try decoder.decode(XCFrameworkInfoPlist_V1.self, from: plistData)
 
-        guard let library = xcFrameworkInfo.availableLibraries.first(where: {
-            if let environment {
-                guard let supportedPlatformVariant = $0.supportedPlatformVariant, supportedPlatformVariant == environment.rawValue else {
-                    return false
+        guard let library = xcFrameworkInfo.findLibrary(platform: platform.rawValue, platformVariant: environment?.rawValue ?? "") else {
+            throw ValidationError("While building for \(platformDisplayName), no library for this platform was found in '\(sourcePath.lastPathComponent)'.")
+        }
+
+        print("[*] library: \(library)")
+        let rootPathToLibrary = sourcePath.join(library.libraryIdentifier)
+        let copyLibraryFromPath = rootPathToLibrary.join(library.libraryPath)
+
+        let copyLibraryToPath = targetPath
+        let libraryTargetPath = copyLibraryToPath.join(library.libraryPath)
+
+        if !fs.fileExists(atPath: copyLibraryFromPath.path) {
+            throw ValidationError("When building for \(platformDisplayName), the expected library \(copyLibraryFromPath.path) was not found in \(sourcePath.path)")
+        }
+
+        try fs.createDirectory(at: targetPath, withIntermediateDirectories: true)
+        try fs.copyItemWithOverwrite(at: copyLibraryFromPath, to: libraryTargetPath)
+
+        if let debugSymbolsPath = library.debugSymbolsPath {
+            let copyDebugSymbolsFromPath = rootPathToLibrary.join(debugSymbolsPath)
+            let copyDebugSymbolsToPath = copyLibraryToPath
+            try fs.copyItemWithOverwrite(at: copyDebugSymbolsFromPath, to: copyDebugSymbolsToPath)
+        }
+
+        if let bitcodeSymbolMapsPath = library.bitcodeSymbolMapsPath {
+            let copyBitcodeSymbolMapsFromPath = rootPathToLibrary.join(bitcodeSymbolMapsPath)
+            let copyBitcodeSymbolMapsToPath = copyLibraryToPath
+            try fs.copyItemWithOverwrite(at: copyBitcodeSymbolMapsFromPath, to: copyBitcodeSymbolMapsToPath)
+        }
+
+        if let headersPath = library.headersPath {
+            let copyHeadersFromPath = rootPathToLibrary.join(headersPath)
+            let copyHeadersToPath = copyLibraryToPath.join("include")
+            try fs.copyItemWithOverwrite(at: copyHeadersFromPath, to: copyHeadersToPath)
+        }
+
+        // ["dylib", "a"]
+        if library.libraryPath.hasSuffix("dylib") || library.libraryPath.hasSuffix(".a") {
+            for file in try fs.contentsOfDirectory(atPath: rootPathToLibrary.path) {
+                let filePath = rootPathToLibrary.join(file)
+
+                let swiftExtensions = ["swiftinterface", "swiftmodule", "swiftdoc"]
+                if swiftExtensions.contains(filePath.pathExtension) {
+                    let destinationPath = copyLibraryToPath.join(file)
+                    try fs.copyItemWithOverwrite(at: filePath, to: destinationPath)
                 }
-                return true
             }
-
-            guard $0.supportedPlatform == platform.rawValue, $0.supportedPlatformVariant == nil else {
-                return false
-            }
-
-            return true
-
-        }) else {
-            if let environment {
-                throw ValidationError("Unsupported platform:\(platform.rawValue) or environment:\(environment.rawValue)")
-            } else {
-                throw ValidationError("Unsupported platform:\(platform.rawValue) or ")
-            }
-        }
-
-        let fm = FileManager.default
-        let libraryRootURL = rootURL.appendingPathComponent(library.libraryIdentifier)
-        let libraryPath = libraryRootURL.appendingPathComponent(library.libraryPath)
-        let targetURL = targetRootURL.appendingPathComponent(library.libraryPath)
-        try fm.copyItemWithOverwrite(at: libraryPath, to: targetURL)
-
-        guard let headersPath = library.headersPath else {
-            return
-        }
-        let headersURL = libraryRootURL.appendingPathComponent(headersPath)
-        if !fm.fileExists(atPath: headersURL.path) {
-            return
-        }
-
-        let targetHeadersURL = targetRootURL.appendingPathComponent("include")
-
-        var isDirectory: ObjCBool = false
-
-        // 检查路径是否存在并且是文件夹
-        if !fm.fileExists(atPath: targetHeadersURL.path, isDirectory: &isDirectory) {
-            try fm.createDirectory(at: targetHeadersURL, withIntermediateDirectories: true)
-        }
-
-        let contents = try fm.contentsOfDirectory(at: headersURL, includingPropertiesForKeys: nil, options: [])
-        for fileURL in contents {
-            let targetFileURL = targetHeadersURL.appendingPathComponent(fileURL.lastPathComponent)
-            try fm.copyItemWithOverwrite(at: fileURL, to: targetFileURL)
         }
     }
 }
 
 import Foundation
+
+extension URL {
+    func join(_ path: String) -> URL {
+        if #available(macOS 13.0, *) {
+            return self.appending(components: path)
+        } else {
+            return appendingPathComponent(path)
+        }
+    }
+}
 
 extension FileManager {
     /// 拷贝文件并覆盖目标文件（如果目标文件已存在）
@@ -131,10 +135,11 @@ extension FileManager {
     ///   - destinationURL: 目标文件的路径
     /// - Throws: 如果发生错误，会抛出异常
     func copyItemWithOverwrite(at sourceURL: URL, to destinationURL: URL) throws {
-        print("[*] copying \(sourceURL.path) to \(destinationURL.path)")
         if fileExists(atPath: destinationURL.path) {
+            print("[*] Removing \(destinationURL.path)")
             try removeItem(at: destinationURL)
         }
+        print("[*] Copying \(sourceURL.path) to \(destinationURL.path)")
         try copyItem(at: sourceURL, to: destinationURL)
     }
 }
--- Sources/xcode-builtin-process-xcframework/XCFrameworkInfo.swift
+++ Sources/xcode-builtin-process-xcframework/XCFrameworkInfo.swift
@@ -7,34 +7,107 @@
 
 import Foundation
 
-struct XCFrameworkInfo: Codable {
-    let availableLibraries: [AvailableLibrary]
-    let cfBundlePackageType: String
-    let xcFrameworkFormatVersion: String
+struct XCFrameworkInfoPlist_V1: Codable {
+    struct Library: Codable {
+        let libraryIdentifier: String
+        let supportedPlatform: String
+        let supportedArchitectures: [String]
+        let platformVariant: String?
+        let libraryPath: String
+        // This is optional because XCFrameworks created with Xcode 14.x and earlier will not define it, but should still be usable.
+        let binaryPath: String?
+        let headersPath: String?
+        let debugSymbolsPath: String?
+        // This is optional because we only want to encode it if the XCFramework is at least of the version which supports it, but this struct doesn't know what that is, so we capture that characteristic in XCFramework.serialize() where the version is available.
+        let mergeableMetadata: Bool?
+        let bitcodeSymbolMapsPath: String?
+
+        enum CodingKeys: String, CodingKey {
+            case libraryIdentifier = "LibraryIdentifier"
+            case supportedPlatform = "SupportedPlatform"
+            case supportedArchitectures = "SupportedArchitectures"
+            case platformVariant = "SupportedPlatformVariant"
+            case libraryPath = "LibraryPath"
+            case binaryPath = "BinaryPath"
+            case headersPath = "HeadersPath"
+            case debugSymbolsPath = "DebugSymbolsPath"
+            case mergeableMetadata = "MergeableMetadata"
+            case bitcodeSymbolMapsPath = "BitcodeSymbolMapsPath"
+        }
+
+        init(libraryIdentifier: String, supportedPlatform: String, supportedArchitectures: [String], platformVariant: String?, libraryPath: String, binaryPath: String?, headersPath: String?, debugSymbolsPath: String?, mergeableMetadata: Bool?, bitcodeSymbolMapsPath: String?) {
+            self.libraryIdentifier = libraryIdentifier
+            self.supportedPlatform = supportedPlatform
+            self.supportedArchitectures = supportedArchitectures
+            self.platformVariant = platformVariant
+            self.libraryPath = libraryPath
+            self.binaryPath = binaryPath
+            self.headersPath = headersPath
+            self.debugSymbolsPath = debugSymbolsPath
+            self.mergeableMetadata = mergeableMetadata
+            self.bitcodeSymbolMapsPath = bitcodeSymbolMapsPath
+        }
+
+        // NOTE: The mappings for maccatalyst are so that "macabi" is used as that is what is directly matched in the LC_BUILD_VERSION info.
+
+        func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(libraryIdentifier, forKey: .libraryIdentifier)
+            try container.encode(supportedPlatform, forKey: .supportedPlatform)
+            try container.encode(supportedArchitectures, forKey: .supportedArchitectures)
+
+            if platformVariant == "macabi" {
+                try container.encode("maccatalyst", forKey: .platformVariant)
+            }
+            else {
+                try container.encodeIfPresent(platformVariant, forKey: .platformVariant)
+            }
+
+            try container.encode(libraryPath, forKey: .libraryPath)
+            try container.encode(binaryPath, forKey: .binaryPath)
+            try container.encodeIfPresent(headersPath, forKey: .headersPath)
+            try container.encodeIfPresent(debugSymbolsPath, forKey: .debugSymbolsPath)
+            try container.encodeIfPresent(mergeableMetadata, forKey: .mergeableMetadata)
+            try container.encodeIfPresent(bitcodeSymbolMapsPath, forKey: .bitcodeSymbolMapsPath)
+        }
+
+        init(from decoder: any Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.libraryIdentifier = try container.decode(String.self, forKey: .libraryIdentifier)
+            self.supportedPlatform = try container.decode(String.self, forKey: .supportedPlatform)
+            self.supportedArchitectures = try container.decode([String].self, forKey: .supportedArchitectures)
+            var platformVariant = try container.decodeIfPresent(String.self, forKey: .platformVariant)
+            if platformVariant == "maccatalyst" {
+                platformVariant = "macabi"
+            }
+            self.platformVariant = platformVariant
+
+            self.libraryPath = try container.decode(String.self, forKey: .libraryPath)
+            self.binaryPath = try container.decodeIfPresent(String.self, forKey: .binaryPath)
+            self.headersPath = try container.decodeIfPresent(String.self, forKey: .headersPath)
+            self.debugSymbolsPath = try container.decodeIfPresent(String.self, forKey: .debugSymbolsPath)
+            self.mergeableMetadata = try container.decodeIfPresent(Bool.self, forKey: .mergeableMetadata)
+            self.bitcodeSymbolMapsPath = try container.decodeIfPresent(String.self, forKey: .bitcodeSymbolMapsPath)
+        }
+    }
+
+    let version: String
+    let libraries: [Library]
+
+    let bundleCode: String = "XFWK"
 
     enum CodingKeys: String, CodingKey {
-        case availableLibraries = "AvailableLibraries"
-        case cfBundlePackageType = "CFBundlePackageType"
-        case xcFrameworkFormatVersion = "XCFrameworkFormatVersion"
+        case version = "XCFrameworkFormatVersion"
+        case libraries = "AvailableLibraries"
+        case bundleCode = "CFBundlePackageType"
     }
 }
 
-struct AvailableLibrary: Codable {
-    let binaryPath: String?
-    let headersPath: String?
-    let libraryIdentifier: String
-    let libraryPath: String
-    let supportedArchitectures: [String]
-    let supportedPlatform: String
-    let supportedPlatformVariant: String?
-
-    enum CodingKeys: String, CodingKey {
-        case binaryPath = "BinaryPath"
-        case headersPath = "HeadersPath"
-        case libraryIdentifier = "LibraryIdentifier"
-        case libraryPath = "LibraryPath"
-        case supportedArchitectures = "SupportedArchitectures"
-        case supportedPlatform = "SupportedPlatform"
-        case supportedPlatformVariant = "SupportedPlatformVariant"
+extension XCFrameworkInfoPlist_V1 {
+    func findLibrary(platform: String, platformVariant: String = "") -> Library? {
+        return libraries.filter { lib in
+            // Due to the fact that macro evaluation of empty settings returns empty strings, there is no meaningful distinction between nil and empty here.
+            lib.supportedPlatform == platform && (lib.platformVariant ?? "") == platformVariant
+        }.first
     }
 }
